8144923f96f34f9030603bc3bc045df46d3f9d24
mfmotta <mariele.f.motta@gmail.com>
2023-08-21 14:40:31 +0200

    add use ddp by default
    refactor configurations out of train.py and into config/...py

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -1,37 +1,56 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
+#I/O
 out_dir = 'out-shakespeare-char'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
+eval_only = False # if True, script exits right after the first eval
 log_interval = 20 # don't print too too often
 
-# we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = True
-
-
+# data
 dataset = 'shakespeare_char' 
 gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
-# baby GPT model :)
+# model
 n_layer = 6
 n_head = 6
 n_embd = 384
-dropout = 0.2
+dropout = 0.2 
+bias = False # TODO do we use bias inside LayerNorm and Linear layers?
+flash = True
+
 # use of flash attention
 flash = False
 
+# adamw optimizer
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 400 #MM:5000
-lr_decay_iters = max_iters # 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
+max_iters = 400 
+weight_decay = 1e-1
+beta1 = 0.9
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 
+# learning rate decay settings
 warmup_iters = max(10, max_iters // 100) # not super necessary potentially
+min_lr = 1e-4 # learning_rate / 10 usually
+decay_lr = True # whether to decay the learning rate
+lr_decay_iters = max_iters # 5000 # make equal to max_iters usually
 
 
+# we expect to overfit on this small dataset, so only save when val improves
+always_save_checkpoint = False
+
+init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+
+
+
+
+
+
+# wandb logging
 wandb_log = False # override via command line if you like
 wandb_project = 'profile-nano-gpt-shakespeare' 
 

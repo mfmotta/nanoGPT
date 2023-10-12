412c99e1e714bb37abaf4782ca4a4033ccca3d2a
mfmotta <mariele.f.motta@gmail.com>
2023-10-12 14:57:59 +0000

    import all parameters from config/train_shakespeare_char.py explicitly as params in train_torch_profiler.py

--- train_torch_profiler.py
+++ train_torch_profiler.py
@@ -25,6 +25,7 @@ from contextlib import nullcontext
 
 import numpy as np
 import torch
+import config.train_shakespeare_char as params
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed import init_process_group, destroy_process_group
 
@@ -33,9 +34,6 @@ from model import GPTConfig, GPT
 # https://github.com/pytorch/kineto/issues/726
 os.environ.update({'KINETO_LOG_LEVEL' : '3'})
 
-# DDP settings
-backend = 'nccl' # 'nccl', 'gloo', etc. 
-
 # system
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
@@ -43,9 +41,9 @@ compile = True # use PyTorch 2.0 to compile the model to be faster
 
 # -----------------------------------------------------------------------------
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
-exec(open('configurator.py').read()) # overrides from command line or config file
-config = {k: globals()[k] for k in config_keys} # will be useful for logging
-print('\n config', config)
+#exec(open('configurator.py').read()) # overrides from command line or config file #MM:not using this
+wandb_config = {k: globals()[k] for k in config_keys} # will be useful for logging
+print('\n wandb_config', wandb_config)
 print('\n check device',torch.cuda.is_available(), torch.cuda.device_count(), torch.cuda.device(0), torch.cuda.get_device_name(0))
 # -----------------------------------------------------------------------------
 
@@ -57,15 +55,15 @@ if ddp:
     ddp_rank = 0 
     ddp_local_rank = 0 
     ddp_world_size = 1 
-    init_process_group(backend=backend, rank=ddp_rank, world_size=ddp_world_size)
+    init_process_group(backend='', rank=ddp_rank, world_size=ddp_world_size)
     device = f'cuda:{ddp_local_rank}'
     torch.cuda.set_device(device)
     master_process = ddp_rank  == 0 
     seed_offset = ddp_rank # MM TODO could exclude this I think
     # world_size number of processes will be training simultaneously, so we can scale
     # down the desired gradient accumulation iterations per process proportionally
-    assert gradient_accumulation_steps % ddp_world_size == 0
-    gradient_accumulation_steps //= ddp_world_size
+    assert params.gradient_accumulation_steps % ddp_world_size == 0
+    params.gradient_accumulation_steps //= ddp_world_size
     print('\n MM Running as DDP with rank = {} and world size = {}'.format(ddp_local_rank, ddp_world_size))
 else:
     # if not ddp, we are running on a single gpu, and one process
@@ -73,11 +71,11 @@ else:
     master_process = True
     seed_offset = 0
     ddp_world_size = 1
-tokens_per_iter = gradient_accumulation_steps * ddp_world_size * batch_size * block_size
+tokens_per_iter = params.gradient_accumulation_steps * ddp_world_size * params.batch_size * params.block_size
 print(f"tokens per iteration will be: {tokens_per_iter:,}")
 
 if master_process:
-    os.makedirs(out_dir, exist_ok=True)
+    os.makedirs(params.out_dir, exist_ok=True)
 
 torch.manual_seed(1337 + seed_offset)
 torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
@@ -89,14 +87,14 @@ ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torc
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # poor woman's data loader TODO: use distributed sampler? https://github.com/pytorch/examples/blob/main/distributed/ddp-tutorial-series/multigpu.py#L87
-data_dir = os.path.join('data', dataset)
+data_dir = os.path.join('data', params.dataset)
 train_data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
 val_data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
 def get_batch(split):
     data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
-    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+    ix = torch.randint(len(data) - params.block_size, (params.batch_size,))
+    x = torch.stack([torch.from_numpy((data[i:i+params.block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+params.block_size]).astype(np.int64)) for i in ix])
     if device_type == 'cuda':
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
@@ -118,10 +116,10 @@ if os.path.exists(meta_path):
     print(f"found vocab_size = {meta_vocab_size} (inside {meta_path})")
 
 # model init
-model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
-                  bias=bias, vocab_size=None, dropout=dropout, flash=flash) # start with model_args from command line
+model_args = dict(n_layer=params.n_layer, n_head=params.n_head, n_embd=params.n_embd, block_size=params.block_size,
+                  bias=params.bias, vocab_size=None, dropout=params.dropout, flash=params.flash) # start with model_args from command line
 
-if init_from == 'scratch':
+if params.init_from == 'scratch':
     # init a new model from scratch
     print("Initializing a new model from scratch")
     # determine the vocab size we'll use for from-scratch training
@@ -131,10 +129,10 @@ if init_from == 'scratch':
     gptconf = GPTConfig(**model_args)
     model = GPT(gptconf)
 
-elif init_from == 'resume':
+elif params.init_from == 'resume':
     print(f"Resuming training from {out_dir}")
     # resume training from a checkpoint.
-    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+    ckpt_path = os.path.join(params.out_dir, 'ckpt.pt')
     checkpoint = torch.load(ckpt_path, map_location=device)
     checkpoint_model_args = checkpoint['model_args']
     # force these config attributes to be equal otherwise we can't even resume training
@@ -155,16 +153,16 @@ elif init_from == 'resume':
     iter_num = checkpoint['iter_num']
     print('\n warm start from iter_num \n ',iter_num)
     best_val_loss = checkpoint['best_val_loss']
-elif init_from.startswith('gpt2'):
-    print(f"Initializing from OpenAI GPT-2 weights: {init_from}")
+elif params.init_from.startswith('gpt2'):
+    print(f"Initializing from OpenAI GPT-2 weights: {params.init_from}")
     # initialize from OpenAI GPT-2 weights
-    override_args = dict(dropout=dropout)
-    model = GPT.from_pretrained(init_from, override_args)
+    override_args = dict(dropout=params.dropout)
+    model = GPT.from_pretrained(params.init_from, override_args)
     # read off the created config params, so we can store them into checkpoint correctly
     for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'bias', 'vocab_size', 'flash']:
         model_args[k] = getattr(model.config, k)
 # crop down the model block size if desired, using model surgery
-if block_size < model.config.block_size:
+if params.block_size < model.config.block_size:
     model.crop_block_size(block_size)
     model_args['block_size'] = block_size # so that the checkpoint will have the right value
 model.to(device)
@@ -173,8 +171,11 @@ model.to(device)
 scaler = torch.cuda.amp.GradScaler(enabled=(dtype == 'float16'))
 
 # optimizer
-optimizer = model.configure_optimizers(weight_decay, learning_rate, (beta1, beta2), device_type)
-if init_from == 'resume':
+optimizer = model.configure_optimizers(params.weight_decay, 
+                                       params.learning_rate, 
+                                       (params.beta1, params.beta2), 
+                                       device_type)
+if params.init_from == 'resume':
     optimizer.load_state_dict(checkpoint['optimizer'])
 checkpoint = None # free up memory
 
@@ -195,8 +196,8 @@ def estimate_loss():
     out = {}
     model.eval()
     for split in ['train', 'val']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
+        losses = torch.zeros(params.eval_iters)
+        for k in range(params.eval_iters):
             X, Y = get_batch(split)
             with ctx:
                 logits, loss = model(X, Y)
@@ -208,16 +209,16 @@ def estimate_loss():
 # learning rate decay scheduler (cosine with warmup)
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
-    if it < warmup_iters:
-        return learning_rate * it / warmup_iters
+    if it < params.warmup_iters:
+        return params.learning_rate * it / params.warmup_iters
     # 2) if it > lr_decay_iters, return min learning rate
-    if it > lr_decay_iters:
-        return min_lr
+    if it > params.lr_decay_iters:
+        return params.min_lr
     # 3) in between, use cosine decay down to min learning rate
-    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    decay_ratio = (it - params.warmup_iters) / (params.lr_decay_iters - params.warmup_iters)
     assert 0 <= decay_ratio <= 1
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff ranges 0..1
-    return min_lr + coeff * (learning_rate - min_lr)
+    return params.min_lr + coeff * (params.learning_rate - params.min_lr)
 
 
 
@@ -228,28 +229,20 @@ local_iter_num = 0 # number of iterations in the lifetime of this process
 raw_model = model.module if ddp else model # unwrap DDP container if needed
 running_mfu = -1.0
 
-#wrap training in the profiler context manager:
-profiler_schedule_args = {
-    'skip_first': 5,
-    'wait': warmup_iters // 2,
-    'warmup': warmup_iters // 2,
-    'active': 3,
-    'repeat': 2
-}
 
 #create a separate logs folder for each attention run
-attention_type = 'flash_' if flash else 'slow_'
-log_folder_name = attention_type+"_".join([f"{key}{value}" for key, value in profiler_schedule_args.items()])
+attention_type = 'flash_' if params.flash else 'slow_'
+log_folder_name = attention_type+"_".join([f"{key}{value}" for key, value in params.profiler_schedule_args.items()])
 log_folder_name = f"logs_{log_folder_name}"
 if master_process:
-    os.makedirs(out_dir+'/'+log_folder_name, exist_ok=True)
+    os.makedirs(params.out_dir+'/'+log_folder_name, exist_ok=True)
 
-if wandb_log and master_process:
+if params.wandb_log and master_process:
     import wandb
    # wandb.tensorboard.patch(root_logdir=out_dir) #+'/'+log_folder_name)
-    wandb.init(project=wandb_project, name=wandb_run_name, config=config, sync_tensorboard=True) 
+    wandb.init(project=params.wandb_project, name=params.wandb_run_name, config=wandb_config, sync_tensorboard=True) 
 
-# Enable profiler with context manager
+# Wrap train loop in the profiler context manager:
 print('\n torch.profiler.itt.is_available()',torch.profiler.itt.is_available())
 with torch.profiler.profile(
 
@@ -258,9 +251,9 @@ with torch.profiler.profile(
         torch.profiler.ProfilerActivity.CUDA,
     ],
 
-    schedule = torch.profiler.schedule(**profiler_schedule_args),
+    schedule = torch.profiler.schedule(**params.profiler_schedule_args),
 
-    on_trace_ready = torch.profiler.tensorboard_trace_handler(out_dir+'/'+log_folder_name, worker_name="gpu0"),#callable that is called at each step when schedule returns ProfilerAction.RECORD_AND_SAVE during the profiling.
+    on_trace_ready = torch.profiler.tensorboard_trace_handler(params.out_dir+'/'+log_folder_name, worker_name="gpu0"),#callable that is called at each step when schedule returns ProfilerAction.RECORD_AND_SAVE during the profiling.
     with_stack = True,
     with_flops = True,
     with_modules = True   
@@ -268,15 +261,15 @@ with torch.profiler.profile(
     
     while True:
         # determine and set the learning rate for this iteration
-        lr = get_lr(iter_num) if decay_lr else learning_rate
+        lr = get_lr(iter_num) if params.decay_lr else params.learning_rate
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
 
         # evaluate the loss on train/val sets and write checkpoints
-        if iter_num % eval_interval == 0 and master_process:
+        if iter_num % params.eval_interval == 0 and master_process:
             losses = estimate_loss()
             print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
-            if wandb_log:
+            if params.wandb_log:
                 wandb.log({
                     "iter": iter_num,
                     "train/loss": losses['train'],
@@ -293,34 +286,34 @@ with torch.profiler.profile(
                         'model_args': model_args,
                         'iter_num': iter_num,
                         'best_val_loss': best_val_loss,
-                        'config': config,
+                        'config': wandb_config,
                     }
                     print(f"saving checkpoint to {out_dir}")
                     torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
-        if iter_num == 0 and eval_only:
+        if iter_num == 0 and params.eval_only:
             break
 
         # forward backward update, with optional gradient accumulation to simulate larger batch size
         # and using the GradScaler if data type is float16
-        for micro_step in range(gradient_accumulation_steps):
+        for micro_step in range(params.gradient_accumulation_steps):
             if ddp:
                 # in DDP training we only need to sync gradients at the last micro step.
                 # the official way to do this is with model.no_sync() context manager, but
                 # I really dislike that this bloats the code and forces us to repeat code
                 # looking at the source of that context manager, it just toggles this variable
-                model.require_backward_grad_sync = (micro_step == gradient_accumulation_steps - 1)
+                model.require_backward_grad_sync = (micro_step == params.gradient_accumulation_steps - 1)
             with ctx:
                 logits, loss = model(X, Y)
-                loss = loss / gradient_accumulation_steps # scale the loss to account for gradient accumulation
+                loss = loss / params.gradient_accumulation_steps # scale the loss to account for gradient accumulation
             # immediately async prefetch next batch while model is doing the forward pass on the GPU
             X, Y = get_batch('train')
             # backward pass, with gradient scaling if training in fp16
             scaler.scale(loss).backward()
             
         # clip the gradient
-        if grad_clip != 0.0:
+        if params.grad_clip != 0.0:
             scaler.unscale_(optimizer)
-            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), params.grad_clip)
         # step the optimizer and scaler if training in fp16
         scaler.step(optimizer)
         scaler.update()
@@ -331,25 +324,25 @@ with torch.profiler.profile(
         t1 = time.time()
         dt = t1 - t0
         t0 = t1
-        if iter_num % log_interval == 0 and master_process:
+        if iter_num % params.log_interval == 0 and master_process:
             # get loss as float. note: this is a CPU-GPU sync point
             # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
-            lossf = loss.item() * gradient_accumulation_steps
+            lossf = loss.item() * params.gradient_accumulation_steps
             if local_iter_num >= 5: # let the training loop settle a bit
-                mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
+                mfu = raw_model.estimate_mfu(params.batch_size * params.gradient_accumulation_steps, dt)
                 running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
             print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
         iter_num += 1
         local_iter_num += 1
 
         # termination conditions
-        if iter_num > max_iters:
+        if iter_num > params.max_iters:
             break
 
         profiler.step()
     profiler.stop()
-    for cycle in glob.glob(f"{out_dir+'/'+log_folder_name}/*.pt.trace.json", recursive=True):
-        wandb.save(cycle, base_path=f"{out_dir+'/'+log_folder_name}", policy="now") 
+    for cycle in glob.glob(f"{params.out_dir+'/'+log_folder_name}/*.pt.trace.json", recursive=True):
+        wandb.save(cycle, base_path=f"{params.out_dir+'/'+log_folder_name}", policy="now") 
     wandb.finish()
             
 if ddp:
